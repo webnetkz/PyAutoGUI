9f97103861038df0c73d98b160b2e2eb91eb3eb8
webnetkz <8gg001@gmail.com>
2020-09-26 03:32:54 +0600

    new library

---
+++ pyautogui.py
@@ -0,0 +1,12 @@
+import pyautogui as pg
+
+#pg.moveTo(200, 300, 0.5)
+#pg.move(1000, 1000, 1)
+#pg.drag(100, 100, 0.5, button='left')
+#pg.click(99, 99)
+#pg.leftClick(5, 5)
+#pg.rightClick()
+#pg.dounbleClick()
+#pg.position()
+#pg.typewrite("Hello world", 0)
+#pg.screenshot("scr.png")
